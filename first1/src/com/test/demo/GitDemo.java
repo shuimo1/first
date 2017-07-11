219057c6009daf2f769f8765331795cd5dcc5a79
Administrator <Administrator@A-201702151126>
2017-07-11 21:11:07 +0800

    x=0003

--- first1/src/com/test/demo/GitDemo.java
+++ first1/src/com/test/demo/GitDemo.java
@@ -1,10 +1,13 @@
 package com.test.demo;
 
 public class GitDemo {
-	
+	public static void test(){
+		System.out.println("这是一个方法");
+	}
 	public static void main(String[] args) {
 		System.out.println("-----------");
 		int sum=20;
 		System.out.println(sum);
+		test();
 	}
 }
