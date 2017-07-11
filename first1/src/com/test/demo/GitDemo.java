8535ecfbe04fc3af1a19ca19f4af37aac7b83494
Administrator <Administrator@A-201702151126>
2017-07-11 21:09:27 +0800

    gitdemo class main() sum=20

---
+++ first1/src/com/test/demo/GitDemo.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+public class GitDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("-----------");
+		int sum=20;
+		System.out.println(sum);
+	}
+}
